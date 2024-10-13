dd430a1056a7b6d1dc7827095b3a863e7b5f404c
melbouman <ameliabouman@my.capilanou.ca>
2024-10-13 15:26:57 -0700

    changes

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,29 @@ $(document).ready(function() {
     // slideToggle("fast");
   });
 });
+var logoarray = [
+  "https://assets.codepen.io/316293/valentina-k-logo-1.svg",
+  "https://assets.codepen.io/316293/valentina-k-logo-2.svg",
+  "https://assets.codepen.io/316293/valentina-k-logo-3.svg"
+];
+
+function getRandomLogo() {
+  var index = Math.floor(Math.random() * logoarray.length);
+  return logoarray[index];
+}
+
+$("#logo-wrapper").hover(function () {
+  var image = getRandomLogo();
+  $("#logoarray").attr("src", image);
+  console.log(image);
+});
+
+// Reset images on mouseout
+
+$("#logo-wrapper").mouseout(function () {
+  $("#logoarray").attr(
+    "src",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/316293/beer.svg"
+  );
+  console.log(image);
+});
